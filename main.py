e77d2de20889762ec677a2c1342bcd9a7df099a8
manulavishvajith <manulap7@gmail.com>
2024-04-04 11:46:22 +0530

    changes to functions for correct call

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, jsonify, request
-from functions import scrape_linkedin  
+from linkedIn import scrape_profile
 
 app = Flask(__name__)
 
-@app.route('/scrape', methods=['POST'])
+
+@app.route("/scrape", methods=["POST"])
 def api_scrape():
     content = request.json
-    profile_url = content.get('profile_url')
-    
+    profile_url = content.get("profile_url")
+
     if not profile_url:
-        return jsonify({'error': 'Profile URL is required.'}), 400
+        return jsonify({"error": "Profile URL is required."}), 400
 
     try:
-        data = scrape_linkedin(profile_url)
+        data = scrape_profile(profile_url)
         return jsonify(data), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- post_request.py
+++ post_request.py
@@ -1,14 +1,14 @@
 import requests
 
 # The URL where your Flask API is running
-api_url = 'http://localhost:5000/scrape'
+api_url = 'http://localhost:5001/scrape'
 
 # Profile URL to be scraped
 profile_url = 'https://www.linkedin.com/in/jaliya-kanchana-b1a595210/'
 
 try:
     # The POST request with the profile URL
-    response = requests.post(api_url, json={'profile_url': profile_url})
+    response = requests.post(api_url, json={'profile_urls': [profile_url]})
     
     # Print the response from the server
     print(response.json())
